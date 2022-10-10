7d5ab730c153db88207586154e8c7458de5576c2
jkchoi <kunyjj@gmail.com>
2022-10-10 20:16:06 +0900

    ox users update3

--- test.js
+++ test.js
@@ -2489,8 +2489,8 @@ app.put('/api/s_ox_users_s1_ch01/update/:type', function(req, res) {
     'JOIN s_ox_users_s3_ch01 ON s_ox_users_s3_ch01.user_id = s_ox_users_s1_ch01.user_id ' +
     'JOIN s_ox_users_s4_ch01 ON s_ox_users_s4_ch01.user_id = s_ox_users_s1_ch01.user_id ' +
     'JOIN s_ox_users_s5_ch01 ON s_ox_users_s5_ch01.user_id = s_ox_users_s1_ch01.user_id ' +
-    'SET "s_ox_users_s"??"_ch01".??=? WHERE s_ox_users_s1_ch01.user_id=?';
-    var params = [qst_pre, order_table, qst_post, col_num, solve_result, type]
+    'SET ??.??=? WHERE s_ox_users_s1_ch01.user_id=?';
+    var params = [order_table, col_num, solve_result, type]
     conn.query(sql, params, function(err, rows, fields) {
         if (err) {
             console.log(err);
