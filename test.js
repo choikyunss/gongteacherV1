725f4cf16f6c29319bf538a656c903302f3fdeec
jkchoi <kunyjj@gmail.com>
2022-10-24 19:51:24 +0900

    user info modify

--- test.js
+++ test.js
@@ -59,7 +59,7 @@ function getFormatDate(date){
     var year = date.getFullYear();
     var month = (1 + date.getMonth());
     month = month >= 10 ? month : '0' + month;
-    var day = date.getDay();
+    var day = date.getDate();
     day = day >= 10 ? day : '0' + day;
 
     return year + "-" + month + "-" + day;
