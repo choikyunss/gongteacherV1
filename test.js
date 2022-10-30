bbeb8bb0b7c2a52dac3cf70a6029bb7f6e765765
jkchoi <kunyjj@gmail.com>
2022-10-30 15:42:04 +0900

    app test

--- test.js
+++ test.js
@@ -1075,7 +1075,7 @@ function trigger_ox_sum() {
                     }
                 });
             }
-            console.log(qst5_sum);
+            console.log(table_string + " " + qst_string + " " + qst5_sum);
             var sql2 = 'UPDATE ?? SET ??=? WHERE qst_id=?'
             var params2 = [ch_string, lv_string, qst5_sum, qst_string]
             conn.query(sql2, params2, function(err2, rows2, fields) {
