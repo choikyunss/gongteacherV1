afbbdcf5fe620875f7f553d79fb7d33eab254262
jkchoi <kunyjj@gmail.com>
2022-10-30 20:56:57 +0900

    app test

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 var mysql = require('mysql');
+const pool = mysql.createPool(conn);
 
 
 /* ********** Make transaction object ********** */
