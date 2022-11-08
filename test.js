2e5c51cd47f6819d7acd36955933876cc0726669
jkchoi <kunyjj@gmail.com>
2022-11-08 21:19:08 +0900

    app test7

--- test.js
+++ test.js
@@ -1201,9 +1201,8 @@ async function TestFunction_UserScore() {
 
         try {
             SolveArray_bit[i] = await dbQueryAsync(sql1, params1);
-            SolveArray_int[i] = SolveArray_bit[i].solveResult == 1 ? 1 : 0;
-            var SolveArray_tmp = SolveArray_bit[i].readInt16LE(0);
-            console.log(SolveArray_tmp); // 배점 Array
+            SolveArray_int[i] = SolveArray_bit[i].solveResult == 0x01 ? 1 : 0;
+            console.log(SolveArray_int[i]); // 배점 Array
         } catch (err) {
             console.log(err);
         }
