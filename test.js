ec3660ea3d8d2652e5c6a55b9a2a55b0819ed54a
jkchoi <kunyjj@gmail.com>
2022-11-10 20:25:24 +0900

    mysql2 test

--- test.js
+++ test.js
@@ -83,7 +83,7 @@ app.post('/api/s_users_id_info/add', async (req, res) => {
     var sql = 'INSERT INTO s_users_id_info (login_id, email, join_route, join_date, level, app_version, c_login_date, p_login_date, terms_accept, ad_accept)'
             + ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
     const [rows, result] = await conn.query(sql, [login_id, email, join_route, join_date, level, app_version, join_date, join_date, terms_accept, ad_accept], (err, rows, fields) => {
-        res.json(rows);      
+        //res.json(rows);      
     });
     conn.release(); 
 });
