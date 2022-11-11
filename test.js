812d8a17cdb4fd96aad71c271f6de7a8110deecf
jkchoi <kunyjj@gmail.com>
2022-11-11 22:51:54 +0900

    mysql2 test

--- test.js
+++ test.js
@@ -121,7 +121,8 @@ app.put('/api/s_users_id_info/update1/:type', async(req, res) => {
         await conn.beginTransaction();
         const [rowsA] = await conn.query(sqlA, type);
         console.log(rowsA[0].c_login_date);
-        var p_login_date = rowsA[0].c_login_date.toString();
+        var date = getFormatDate(rowsA[0].c_login_date);
+        var p_login_date = date;
         console.log(p_login_date);
         const [rowsB] = await conn.query(sqlB, params);
         await conn.commit();
